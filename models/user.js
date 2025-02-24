6c3b9e31d941e7758961c46daa4d6877f0744913
sandeep-luhaniwal <sandeepluh0001.com>
2025-02-24 10:31:35 +0530

    first commit

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/project1');
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    email: String,
+    age: Number,
+    password: String,
+    posts: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:"post",
+        }
+    ]
+
+})
+
+module.exports = mongoose.model('user', userSchema);
